ade11170db0047875a5819424e321319128e7ba5
MoazAymanAthr <moaza.ayman.athr@gmail.com>
2025-07-20 01:30:33 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/login_page.dart';
+
+void main() {
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginScreen()));
+}
