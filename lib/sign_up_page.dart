a1f9c723abd9fffa9c0b62495816efbddf096a69
MoazAymanAthr <moaza.ayman.athr@gmail.com>
2025-07-28 11:38:58 +0300

    Refactor signup page to use Bloc for state management, implementing form validation and user feedback on signup success or failure.

--- lib/sign_up_page.dart
+++ lib/sign_up_page.dart
@@ -1,147 +1,168 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/bloc/signup_bloc.dart';
+import 'package:flutter_application_1/home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class SignupScreen extends StatelessWidget {
-  const SignupScreen({super.key});
+class SingUpScreenWithBloc extends StatefulWidget {
+  const SingUpScreenWithBloc({super.key});
+
+  @override
+  State<SingUpScreenWithBloc> createState() => _SingUpScreenWithBlocState();
+}
+
+final _formKey = GlobalKey<FormState>();
+
+final _emailController = TextEditingController();
+final _passwordController = TextEditingController();
+final _confirmPasswordController = TextEditingController();
+
+class _SingUpScreenWithBlocState extends State<SingUpScreenWithBloc> {
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 10),
-                Text(
-                  'Sign up',
-                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 8),
-                Text(
-                  'Create your account',
-                  style: TextStyle(color: Colors.grey[600]),
-                ),
-                SizedBox(height: 32),
-                TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person),
-                    hintText: 'Username',
-                    filled: true,
-                    fillColor: Color(0xffEFE4F3),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email),
-                    hintText: 'Email',
-                    filled: true,
-                    fillColor: Color(0xffEFE4F3),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock),
-                    hintText: 'Password',
-                    filled: true,
-                    fillColor: Color(0xffEFE4F3),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock_outline),
-                    hintText: 'Confirm Password',
-                    filled: true,
-                    fillColor: Color(0xffEFE4F3),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 24),
-                SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Color(0XFF9B28B2),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                    ),
-                    child: Text(
-                      'Sign up',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 16),
-                Center(child: Text("Or")),
-                SizedBox(height: 12),
-                SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    style: OutlinedButton.styleFrom(
-                      foregroundColor: Color(0xFF9A11FF),
-                      side: BorderSide(color: Color(0xFF9A11FF)),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      ),
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                    ),
-                    child: Text('Sign In with Google'),
-                  ),
-                ),
-                SizedBox(height: 20),
+    return BlocProvider(
+      create: (context) => SignUpBloc(),
 
-                Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: RichText(
-                      text: TextSpan(
-                        text: "Already have an account? ",
-                        style: TextStyle(color: Colors.black),
-                        children: [
-                          TextSpan(
-                            text: 'Login',
-                            style: TextStyle(
-                              color: Color(0xFF9A11FF),
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
+      child: BlocConsumer<SignUpBloc, SignUpState>(
+        listener: (context, state) {
+          if (state is SignUpFailure) {
+            Fluttertoast.showToast(
+              msg: state.error,
+              toastLength: Toast.LENGTH_LONG,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.red,
+              textColor: Colors.white,
+            );
+          }
+          if (state is SignUpSuccess) {
+            Fluttertoast.showToast(
+              msg: 'Sign up successful! Welcome ${state.email}',
+              toastLength: Toast.LENGTH_LONG,
+              gravity: ToastGravity.BOTTOM,
+              backgroundColor: Colors.green,
+              textColor: Colors.white,
+            );
+            Future.delayed(const Duration(seconds: 1), () {
+              if (context.mounted) {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(userEmail: state.email),
+                  ),
+                );
+              }
+            });
+          }
+        },
+        builder: (context, state) {
+          if (state is SignUpLoading) {
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
+            );
+          }
+          return Scaffold(
+            body: Center(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 40,
+                ),
+                child: Form(
+                  autovalidateMode: AutovalidateMode.disabled,
+                  key: _formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        controller: _emailController,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          border: const OutlineInputBorder(),
+                          prefixIcon: const Icon(Icons.email),
+                          errorText:
+                              state is SignUpValidationState
+                                  ? state.emailError
+                                  : null,
+                        ),
+                        keyboardType: TextInputType.emailAddress,
+                        onChanged: (value) {
+                          context.read<SignUpBloc>().add(
+                            SignUpEmailChangedEvent(value),
+                          );
+                        },
                       ),
-                    ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _passwordController,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          border: const OutlineInputBorder(),
+                          prefixIcon: const Icon(Icons.lock),
+                          errorText:
+                              state is SignUpValidationState
+                                  ? state.passwordError
+                                  : null,
+                        ),
+                        obscureText: true,
+                        onChanged: (value) {
+                          context.read<SignUpBloc>().add(
+                            SignUpPasswordChangedEvent(value),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      TextFormField(
+                        controller: _confirmPasswordController,
+                        decoration: InputDecoration(
+                          labelText: 'Confirm Password',
+                          border: const OutlineInputBorder(),
+                          prefixIcon: const Icon(Icons.lock),
+                          errorText:
+                              state is SignUpValidationState
+                                  ? state.confirmPasswordError
+                                  : null,
+                        ),
+                        obscureText: true,
+                        onChanged: (value) {
+                          context.read<SignUpBloc>().add(
+                            SignUpConfirmPasswordChangedEvent(value),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 30),
+                      SizedBox(
+                        height: 50,
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            context.read<SignUpBloc>().add(
+                              SignUpSubmittedEvent(
+                                email: _emailController.text.trim(),
+                                password: _passwordController.text.trim(),
+                                confirmPassword:
+                                    _confirmPasswordController.text.trim(),
+                              ),
+                            );
+                          },
+                          child: const Text('Sign Up'),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 20),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
